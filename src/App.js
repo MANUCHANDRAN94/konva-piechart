638fb5e219f4ecaf7deebdb0622eb5795533a2f9
MANUCHANDRAN94 <m4manu001@gmail.com>
2020-10-26 15:29:42 +0530

    /** *TYPE: new feature 🛠 *SUBJECT: piechart scale and transform * BODY:  Transform function h*w ratio need to fix */

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { Layer, Stage, Text } from 'react-konva';
+import PieGroup from './components/PieGroup';
+
+
+const initialValues = {
+  id: 0,
+  x: 100,
+  y: 20,
+  height: 400,
+  width: 400,
+  // radius: ,
+  dataCollection: [
+    { product: "apple", value: 120, color: "red", strokeColor: "white", strokeWidth: 2 },
+    { product: "mango", value: 140, color: "yellow", strokeColor: "white", strokeWidth: 2 },
+    { product: "guvava", value: 130, color: "green", strokeColor: "white", strokeWidth: 2 },
+    { product: "grape", value: 100, color: "blue", strokeColor: "white", strokeWidth: 2 },
+  ]
+}
 
 function App() {
+
+  const [graphDetails, setGraphDetails] = useState(initialValues);
+  const [selectedId, selectShape] = useState(null);
+
+  useEffect(() => {
+    setGraphDetails(preValue => {
+      return {
+        ...initialValues
+      }
+    })
+  }, [])
+
+  const checkDeselect = (e) => {
+    // deselect when clicked on empty area
+    const clickedOnEmpty = e.target === e.target.getStage();
+    if (clickedOnEmpty) {
+      selectShape(null);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="pieChartContainer">
+      <Stage x={0} y={0} width={window.innerWidth} height={window.innerHeight} onMouseDown={checkDeselect} onTouchStart={checkDeselect}>
+        <Layer>
+          <Text x={10} width={(window.innerWidth - 200)} align="center" text="Pie Chart" stroke="black" fontSize={36} />
+          <PieGroup groupValue={graphDetails}
+            isSelected={graphDetails.id === selectedId}
+            onSelect={() => {
+              selectShape(graphDetails.id);
+            }}
+            onChange={(newAttrs) => {
+              // const rects = rectangles.slice();
+              // rects[i] = newAttrs;
+              setGraphDetails(newAttrs);
+            }}
+          />
+        </Layer>
+      </Stage>
     </div>
   );
 }
